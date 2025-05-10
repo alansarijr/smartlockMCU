46bb8ebe2a96cd527fe1355565ca8fdd2547041d
mohamedselansari@gmail.com <ms019070@gmail.com>
2025-05-10 23:14:27 +0300

    dd

--- src/main.c
+++ src/main.c
@@ -8,27 +8,85 @@
 #include "../lib/KEYPAD/KEYPAD.h"
 #include "../lib/EEPROM/EEPROM.h"
 #include <../lib/SERVO/SERVO.h>
+
+// Function Prototypes
+void initializeEEPROM(unsigned char *pass);
+void resetPass();
+void init_project();
+void print_welcome();
+int enter_password();
+
 char correct_pass[5] = "1234"; // Default password
-void initializeEEPROM(unsigned char *pass)
+unsigned int people_count = 0; // Initialize people count
+
+int main(void)
 {
-    // Initialize EEPROM with default password
-    // Check if EEPROM is empty (or contains a specific value)
-    // If empty, write the default password to EEPROM
-    if (EEPROM_read(0x0) == 0xFF) // Assuming 0xFF indicates empty
+    // Setting Up The Project
+    init_project(); // Initialize servo
+
+    char entered_pass[5] = {0}; // Initialize to zeros
+    uint8_t pass_index = 0;
+
+    print_welcome(); // Print welcome message
+    while (1)
     {
-        for (int i = 0; i < 4; i++)
+
+        // Password Screen
+        char k = keypad_getkey();
+        if (k != 0)
         {
-            EEPROM_write(i, pass[i]);
+            if (k == '*')
+            {
+                // Enter the password
+                int ss = enter_password();
+                if (ss == 1)
+                    people_count++;
+            }
+            else if (k == '+')
+            {
+                // Reset the password
+                resetPass();
+            }
+            else if (k == '-')
+            {
+                // Get out
+            }
         }
+
+        // Enter the password
+
+        // Correct Password Screen
+
+        // Open the door Screen and rotate the servo
+
+        // Wrong Password Screen
+
+        // Reset the password
+
+        // Print the welcome message
+        print_welcome(); // Print welcome message
+        _delay_ms(1000); // Delay for 1 second
     }
-    else
-    {
-        // Read the password from EEPROM into the correct_pass array
-        for (int i = 0; i < 4; i++)
-        {
-            correct_pass[i] = EEPROM_read(i);
-        }
-    }
+}
+
+void print_welcome()
+{
+    // Print welcome message on LCD
+    LCD_gotoXY(0, 1);
+    LCD_SendString("Welcome to SDL");
+    LCD_gotoXY(0, 0);
+    // Print people count on LCD
+    LCD_SendString("People Count:");
+    LCD_SendNumber(people_count);
+}
+
+void init_project()
+{
+    // Initialize the project
+    LCD_Init();                     // Initialize LCD
+    keypad_init();                  // Initialize keypad
+    servo_init();                   // Initialize servo
+    initializeEEPROM(correct_pass); // Initialize EEPROM with default password
 }
 
 void resetPass()
@@ -70,28 +128,40 @@ void resetPass()
     }
 }
 
-int main(void)
+void initializeEEPROM(unsigned char *pass)
 {
-    keypad_init();
-    LCD_Init();
-    initializeEEPROM(correct_pass); // Initialize EEPROM with default password
-    _delay_ms(3000);                // Wait for 1 second
+    // Initialize EEPROM with default password
+    // Check if EEPROM is empty (or contains a specific value)
+    // If empty, write the default password to EEPROM
+    if (EEPROM_read(0x0) == 0xFF) // Assuming 0xFF indicates empty
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            EEPROM_write(i, pass[i]);
+        }
+    }
+    else
+    {
+        // Read the password from EEPROM into the correct_pass array
+        for (int i = 0; i < 4; i++)
+        {
+            correct_pass[i] = EEPROM_read(i);
+        }
+    }
+}
 
-    servo_init();
-    servo_rotate(0);            // Rotate servo to 0 degrees (or any position you want)
-    servo_rotate(90);           // Rotate servo to 90 degrees (or any position you want)
+int enter_password()
+{
+    // LCD Print Enter Password
+    LCD_SendCMD(LCD_CMD_CLEAR_DISPLAY);
+    LCD_SendString("Enter Password:");
+    LCD_gotoXY(0, 1);
     char entered_pass[5] = {0}; // Initialize to zeros
     uint8_t pass_index = 0;
-
+    char k;
     while (1)
     {
-        DDRC = 0xFF;  // Set PORTC as output for LCD
-        PORTC = 0xFF; // Initialize PORTC to 0
-        char k = keypad_getkey();
-        if (k == '+')
-        {
-            resetPass();
-        }
+        k = keypad_getkey();
         if (k != 0)
         {
             _delay_ms(50); // Debounce delay
@@ -102,7 +172,7 @@ int main(void)
                 if (pass_index < 4)
                 {
                     entered_pass[pass_index] = k;
-                    LCD_SendData('*'); // Show '' instead of the actual digit
+                    LCD_SendData('*'); // Show * instead of the actual digit
                     pass_index++;
                 }
             }
@@ -122,19 +192,19 @@ int main(void)
                 {
                     LCD_SendCMD(LCD_CMD_CLEAR_DISPLAY);
                     LCD_SendString("Correct!");
+                    _delay_ms(1500); // Show message for 2 seconds
+                    LCD_SendCMD(LCD_CMD_CLEAR_DISPLAY);
+                    return 1; // Password is correct
                 }
                 else
                 {
                     LCD_SendCMD(LCD_CMD_CLEAR_DISPLAY);
                     LCD_SendString("Wrong!");
+                    _delay_ms(1500); // Show message for 2 seconds
+                    LCD_SendCMD(LCD_CMD_CLEAR_DISPLAY);
+                    return 0; // Password is wrong
                 }
-
-                _delay_ms(1500); // Show message for 2 seconds
-                LCD_SendCMD(LCD_CMD_CLEAR_DISPLAY);
-                pass_index = 0;
-                memset(entered_pass, 0, sizeof(entered_pass));
             }
         }
-        _delay_ms(10); // Reduce CPU usage
     }
 }
